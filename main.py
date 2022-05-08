ca5b61b55c3f178699fa28620275fbacceeeef7f
JC60522 <79722080+JC60522@users.noreply.github.com>
2022-05-08 21:01:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import secrets
+import json, secrets, re
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
@@ -10,6 +10,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from ipfs_interface import Ipfs
+from markupsafe import Markup
 
 
 limiter = Limiter(key_func=get_remote_address)
@@ -18,26 +19,23 @@ security = HTTPBasic()
 
 app = FastAPI(
     title="dcr-timestampbot",
-    description="dcr-timestampbot REST-API.",
+    description="dcr-timestampbot",
     version="0.0.1",
     docs_url=None,
-    openapi_url=None
-)
+    openapi_url=None)
+
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["GET", "POST"],
-    allow_headers=["*"],
 )
 
 
 # Basic auth wall to access swagger docs
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "############")
-    correct_password = secrets.compare_digest(credentials.password, "################")
+    correct_username = secrets.compare_digest(credentials.username, "######")
+    correct_password = secrets.compare_digest(credentials.password, "######")
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -48,13 +46,13 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 
 
 @app.get("/docs", include_in_schema=False)
-async def get_swagger_documentation(request: Request, username: bool = Depends(get_current_username)):
+async def get_swagger_documentation(username: bool = Depends(get_current_username)):
     if username:
         return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
 
 
 @app.get("/openapi.json", include_in_schema=False)
-async def openapi(request: Request, username: bool = Depends(get_current_username)):
+async def openapi(username: bool = Depends(get_current_username)):
     if username:
         return get_openapi(title=app.title, version=app.version, routes=app.routes)
 
@@ -63,30 +61,48 @@ async def openapi(request: Request, username: bool = Depends(get_current_usernam
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 # Render Landing page
 @app.get('/')
-@limiter.limit("10/minute")
+@limiter.limit("5/minute")
 def homepage(request: Request, username: bool = Depends(get_current_username)):
     if username:
-        return templates.TemplateResponse('index.html', {"request":request})
+        return templates.TemplateResponse('index.html', {"request": request})
 
 
 # Route for maunal CID search
 @app.post("/")
-@limiter.limit("10/minute")
+@limiter.limit("5/minute")
 async def form_post(request: Request, _hash: str = Form(...)):
+    if re.match(r'^[Qm][1-9A-Za-z]{44}[^OIl]$', _hash) is None:
+        return templates.TemplateResponse('result.html',
+                                          context={'request': request, 'result': Markup(f'''<b>Bad Query</b>
+                                          <br><a href="https://dcr-timestampbot.com"><b style="color: red; ">
+                                          Back to Home<b></a>''')})
     result = Ipfs(_hash).search()
     if isinstance(result, bytes):
-        result = result.decode("utf-8")  
+        result = result.decode("utf-8")
+        result = Markup(f'<hr size="8"><br><b>{result}</b><br>'
+                        f' <a href="https://twitter.com/{json.loads(result)["user"]["screen_name"]}/status/'
+                        f'{json.loads(result)["id_str"]}"><b style="color: red; ">'
+                        f'View on Twitter<b></a><br><hr size="8">''')
     return templates.TemplateResponse('result.html', context={'request': request, 'result': result})
 
 
 # Route for ipfs-url in tweets
 @app.get("/ipfs/{_hash}")
-@limiter.limit("10/minute")
-async def get(request: Request, _hash: str, username: bool = Depends(get_current_username)):
-    if username:
-        result = Ipfs(_hash).search()
-        if isinstance(result, bytes):
-            result = result.decode("utf-8")
-        return templates.TemplateResponse('result.html', context={'request': request, 'result': result})
+@limiter.limit("5/minute")
+async def get(request: Request, _hash: str):
+    if re.match(r'^[Qm][1-9A-Za-z]{44}[^OIl]$', _hash) is None:
+        return templates.TemplateResponse('result.html',
+                                          context={'request': request, 'result': Markup(f'''<b>Bad Query</b>
+                                                  <br><a href="https://dcr-timestampbot.com"><b style="color: red; ">
+                                                  Back to Home<b></a>''')})
+    result = Ipfs(_hash).search()
+    if isinstance(result, bytes):
+        result = result.decode("utf-8")
+        result = Markup(f'<hr size="8"><br><b>{result}</b><br>'
+                        f' <a href="https://twitter.com/{json.loads(result)["user"]["screen_name"]}/status/'
+                        f'{json.loads(result)["id_str"]}"><b style="color: red; ">'
+                        f'View on Twitter<b></a><br><hr size="8">''')
+    return templates.TemplateResponse('result.html', context={'request': request, 'result': result})
